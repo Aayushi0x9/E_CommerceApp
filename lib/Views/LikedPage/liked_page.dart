ab8c9eea5287e7b43f91788e2626ad7a7d1342d0
Aayushi0x9e <twishabansu7788@gmail.com>
2024-07-10 16:08:43 +0530

    second commit

---
+++ lib/Views/LikedPage/liked_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LikedWallpaperPage extends StatelessWidget {
+  const LikedWallpaperPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
